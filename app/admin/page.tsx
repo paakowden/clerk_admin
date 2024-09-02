cc6c56f34fe9b0d89690f1689fdb921b26a0734d
paakowden <paakow1114@gmail.com>
2024-09-02 18:39:53 +0000

    admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,17 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/dist/server/api-utils";
 import React from "react";
 
 const Admin = async () => {
+  const { userId } = auth();
+  const isAuth = !!userId;
+  const user = await currentUser();
+
+  if (user?.publicMetadata?.role !== "ADMIN") {
+    // Handle non-admin users here
+    return <div>Access Denied</div>;
+  }
+
   return (
     <div>
       <h1>Welcome to Admin Page</h1>
