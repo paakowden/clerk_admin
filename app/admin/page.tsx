3e6c550b3b136b93f4ecba31c1b71b70a63c40db
paakowden <paakow1114@gmail.com>
2024-09-04 10:04:53 +0000

    new_con_string

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,20 +1,24 @@
+import { useUser } from "@clerk/nextjs";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Admin = async () => {
-  const { userId } = auth();
-  const isAuth = !!userId;
-  const user = await currentUser();
+  // const { userId } = auth();
+  //const isAuth = !!userId;
+  // const user = await currentUser();
 
   // if (user?.publicMetadata?.role !== "ADMIN") {
   // Handle non-admin users here
   // return <div>Access Denied</div>;
   //}
 
-  if (!user || user.publicMetadata.role !== "ADMIN") {
-    return redirect("/");
-  }
+  //if (!user || !== "ADMIN") {
+  //  return redirect("/");
+  // }
+
+  //const { user } = useUser()
+  //const isAdmin = user?.publicMetadata?.role = 'ADMIN'
   return (
     <div>
       <h1>Welcome to Admin Page</h1>
